05413c242ac4c113ecb36dd586617d312eb58155
Bhanu Agarwal <57340784+bhanu613@users.noreply.github.com>
2026-07-26 13:28:19 +0200

    Update stylesheet link and header include

--- student/studentdashboard.php
+++ student/studentdashboard.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="..\css\studentdashboard.css">
+    <link rel="stylesheet" href="../css/studentdashboard.css">
 
     <script type="text/javascript">
       function applyleave(){
@@ -24,7 +24,7 @@
 
 <!-- about -->
 
-<?php include '../header.php';?>
+<?php include '../admin/header.php';?>
 
 
 <!-- end about -->
