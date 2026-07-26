387cabc1fec11513bb5b3616bd87d4d4d47e477a
Bhanu Agarwal <57340784+bhanu613@users.noreply.github.com>
2026-07-26 13:25:52 +0200

    Fix stylesheet link path in admindashboard.php

--- admin/admindashboard.php
+++ admin/admindashboard.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="..\css\mhreg.css">
+    <link rel="stylesheet" href="../css/mhreg.css">
 	<style>
 	body {
    width: 100vw;
